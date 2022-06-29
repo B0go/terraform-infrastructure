3dfb450b8db524e29fd9328523012563cf1098e5
B0go <victorbogo@icloud.com>
2022-06-28 22:43:52 -0300

    add backend and provider configuration for dns setup
    Signed-off-by: B0go <victorbogo@icloud.com>

---
+++ terraform/dns/providers.tf
@@ -0,0 +1,11 @@
+provider "aws"{
+    region = "us-east-1"
+    profile = "personal-aws-terraform"
+}
+
+provider "godaddy"{
+}
+
+provider "mailgun" {
+    api_key = "${var.MAILGUN_API_KEY}"
+}
---
+++ terraform/dns/versions.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = "~> 1.2.3"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.20.1"
+    }
+    // For this provider to work on Apple Silicon based macs, terraform for darwin_amd64 should be installed.
+    godaddy = {
+      source = "n3integration/godaddy"
+      version = "1.8.7"
+    }
+    mailgun = {
+      source = "wgebis/mailgun"
+      version = "0.7.2"
+    }
+  }
+}
